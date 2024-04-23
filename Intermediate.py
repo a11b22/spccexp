4ab755dfa83d01ec3fa622cd92ea68789c6ac682
a11b22 <167750796+a11b22@users.noreply.github.com>
2024-04-23 18:52:46 +0530

    Update Intermediate.py

--- Intermediate.py
+++ Intermediate.py
@@ -1,115 +1,73 @@
 '''INTERMEDIATE CODE GENERATOR:'''
-def parse_input(input_string):
 
-    def E():
-        nonlocal cursor
-        print(f"{cursor.ljust(16)} E -> T E'")
-        if T():
-            if Edash():
-                return True
+class ThreeAddressGenerator:
+    def __init__(self):
+        self.temp_count = 1
+
+    def generate_temp(self):
+        temp = f"T{self.temp_count}"
+        self.temp_count += 1
+        return temp
+
+    def postfix_to_three_address(self, postfix):
+        stack = []
+        three_address_code = []
+        for token in postfix:
+            if token.isalnum():
+                stack.append(token)
             else:
-                return False
-        else:
-            return False
+                op2 = stack.pop()
+                op1 = stack.pop()
+                temp = self.generate_temp()
+                three_address_code.append(f"{temp} = {op1} {token} {op2}")
+                stack.append(temp)
+        return three_address_code
 
-
-    def Edash():
-        nonlocal cursor
-        if cursor and cursor[0] in ['+', '-']:
-            print(f"{cursor.ljust(16)} E' -> {cursor[0]} T E'")
-            cursor = cursor[1:]
-            if T():
-                if Edash():
-                    return True
-                else:
-                    return False
+    def infix_to_postfix(self, infix):
+        precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '(': 0}
+        postfix = []
+        stack = []
+        # Remove spaces from the input expression
+        infix = infix.replace(" ", "")
+        for token in infix:
+            if token.isalnum():
+                postfix.append(token)
+            elif token == '(':
+                stack.append(token)
+            elif token == ')':
+                # Pop operators until '(' is encountered
+                while stack and stack[-1] != '(':
+                    postfix.append(stack.pop())
+                stack.pop()  # Remove '('
             else:
-                return False
-        else:
-            print(f"{cursor.ljust(16)} E' -> $")
-            return True
-
-    def T():
-        nonlocal cursor
-        print(f"{cursor.ljust(16)} T -> F T'")
-        if F():
-            if Tdash():
-                return True
-            else:
-                return False
-        else:
-            return False
-
-
-    def Tdash():
-        nonlocal cursor
-        if cursor and cursor[0] in ['*', '/']:
-            print(f"{cursor.ljust(16)} T' -> {cursor[0]} F T'")
-            cursor = cursor[1:]
-            if F():
-                if Tdash():
-                    return True
-                else:
-                    return False
-            else:
-                return False
-        else:
-            print(f"{cursor.ljust(16)} T' -> $")
-            return True
-
-   
-    def F():
-        nonlocal cursor
-        if cursor and cursor[0].isdigit():
-            print(f"{cursor.ljust(16)} F -> {cursor[0]}")
-            cursor = cursor[1:]
-            return True
-        elif cursor and cursor[0] == '(':
-            print(f"{cursor.ljust(16)} F -> ( E )")
-            cursor = cursor[1:]
-            if E():
-                if cursor and cursor[0] == ')':
-                    cursor = cursor[1:]
-                    return True
-                else:
-                    return False
-            else:
-                return False
-        elif cursor and cursor[0] == 'i':
-            print(f"{cursor.ljust(16)} F -> i")
-            cursor = cursor[1:]
-            return True
-        else:
-            return False
-
-    cursor = input_string
-    if E() and not cursor:
-        print("--------------------------------")
-        print("String is successfully parsed")
-        return True
-    else:
-        print("--------------------------------")
-        print("Error in parsing String")
-        return False
-
-
-# Main function
-def main():
-    print("Enter the string:")
-    input_string = input().strip()  
-    print("\nInput          Action")
-    print("--------------------------------")
-    if parse_input(input_string):
-        return 0
-    else:
-        return 1
+                # Pop operators with higher or equal precedence
+                while stack and precedence.get(stack[-1], 0) >= precedence[token]:
+                    postfix.append(stack.pop())
+                stack.append(token)
+        # Append remaining operators from stack
+        postfix.extend(stack[::-1])
+        return postfix
 
 
 if __name__ == "__main__":
-    exit_code = main()
-    exit(exit_code)
- '''   
-OUTPUT
-Enter the string:
-i+(i*i)
-'''
+    generator = ThreeAddressGenerator()
+
+    infix_expr = input("Enter the infix expression: ")
+    postfix_expr = generator.infix_to_postfix(infix_expr)
+    postfix_str = ' '.join(postfix_expr)
+    print("Postfix representation of the expression:", postfix_str)
+
+    postfix_tokens = postfix_expr
+    three_address_code = generator.postfix_to_three_address(postfix_tokens)
+    print("\nThree-address code for the expression:")
+    for code in three_address_code:
+        print(code)
+
+
+# OUTPUT
+# Enter the infix expression: a+b*c
+# Postfix representation of the expression: a b c * +
+
+# Three-address code for the expression:
+# T1 = b * c
+# T2 = a + T1
